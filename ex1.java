7fdaacca23b2a63b1a8167a26af28aad022e1db9
Sebastien Baloge <s.baloge@yahoo.fr>
2022-07-08 13:12:35 +0200

    Initial Commit

---
+++ ex1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Essai
+ */
+public class Essai {
+public static void main(String[] args) {
+Scanner input = new Scanner (System.in);
+System.out.println("Quel est ton prénom ?");       
+String Prenom = input.nextLine();
+System.out.println("Quel est ton nom ?");
+String Nom = input.nextLine();
+
+}
+    
+}
