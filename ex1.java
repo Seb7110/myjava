a56ed567c27b601b2c842cc635f7dcb2e01b5711
Sebastien Baloge <s.baloge@yahoo.fr>
2022-07-08 13:57:43 +0200

    Ajout de la fonction FirstName

--- ex1.java
+++ ex1.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
  */
 public class ex1 {
 public static void main(String[] args) {
-Scanner input = new Scanner (System.in);
 System.out.println("Quel est ton prénom ?");       
-String Prenom = input.nextLine();
+String Prenom =FirstName("Sébastien");
+System.out.println("Quel est ton nom ?");
 String nom = DisplayName("Baloge") ;
 
 }
 static String DisplayName (String name){
 name = "Baloge" ;
 System.out.println("Ton nom est " + name);
+return name ;
+}
+static String FirstName (String name) {
+    name ="Sébastien";
+    System.out.println("Ton prénom est " + name);
 return name ;
 }
     
