47ecf233d09652472bd8216a33167ae035b79b99
Sebastien Baloge <s.baloge@yahoo.fr>
2022-07-08 13:45:12 +0200

    Ajout de la fonction DisplayName

--- ex1.java
+++ ex1.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 /**
  * Essai
  */
-public class Essai {
+public class ex1 {
 public static void main(String[] args) {
 Scanner input = new Scanner (System.in);
 System.out.println("Quel est ton prénom ?");       
 String Prenom = input.nextLine();
-System.out.println("Quel est ton nom ?");
-String Nom = input.nextLine();
+String nom = DisplayName("Baloge") ;
 
+}
+static String DisplayName (String name){
+name = "Baloge" ;
+System.out.println("Ton nom est " + name);
+return name ;
 }
     
 }
