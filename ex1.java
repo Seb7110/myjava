9a424ba14f2200186f125a072740e3abca174a9e
Sebastien Baloge <s.baloge@yahoo.fr>
2022-07-08 14:20:53 +0200

    Get fonction FullName

--- ex1.java
+++ ex1.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
  * Essai
  */
 public class ex1 {
+    static Scanner input = new Scanner (System.in) ;
+
 public static void main(String[] args) {
 System.out.println("Quel est ton prénom ?");       
 String Prenom =FirstName("Sébastien");
 System.out.println("Quel est ton nom ?");
 String nom = DisplayName("Baloge") ;
+String nomComplet = FullName("Sébastien", "Baloge");
 
 }
 static String DisplayName (String name){
@@ -19,6 +22,16 @@ return name ;
 static String FirstName (String name) {
     name ="Sébastien";
     System.out.println("Ton prénom est " + name);
+return name ;
+}
+static String FullName (String firstName, String name) {
+firstName = "Sébastien";
+name = "Baloge";
+
+    System.out.println("Quel est ton prénom ?");
+System.out.println("Ton prénom est " + firstName);
+    System.out.println("Quel est ton nom ?");
+System.out.println("Ton nom est " + name);
 return name ;
 }
     
